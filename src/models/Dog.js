a971404c0330d0b9e1a6da6920028008a2e7d006
dwl5986 <dwl5986@rit.edu>
2016-03-24 03:11:31 -0400

    Added files via upload
    Initial Commit

---
+++ src/models/Dog.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose');
+
+var DogModel;
+
+var DogSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+
+    breed: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    age: {
+        type: Number,
+        min: 0,
+        required: true
+    },
+
+    createdData: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+DogSchema.methods.sayName = function() {
+    console.log(this.name);
+};
+
+DogSchema.statics.findByName = function(name, callback) {
+    var search = {
+        name: name
+    };
+    return DogModel.findOne(search, callback);
+};
+
+DogModel = mongoose.model('Dog', DogSchema);
+
+//export our public properties
+module.exports.DogModel = DogModel;
+module.exports.DogSchema = DogSchema;
